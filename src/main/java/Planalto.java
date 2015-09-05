7d3978ef9c12e30be61a78aa8ef24f654f5d7296
saturno <drsaturno@gmail.com>
2015-09-05 20:30:30 -0300

    Removendo arquivos

--- src/main/java/Planalto.java
+++
@@ -1,9 +0,0 @@
-package main.java;
-
-public class Planalto {
-
-	public Planalto(int i, int j) {
-		// TODO Auto-generated constructor stub
-	}
-	
-}
--- src/main/java/Posicao.java
+++
@@ -1,9 +0,0 @@
-package main.java;
-
-public class Posicao {
-
-	public Posicao(int i, int j, String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
